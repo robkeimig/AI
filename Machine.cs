f5223c302a3fa983c2ff84588b00ac1db63ccf4a
Rob Keimig <Rob.Keimig@pinnstrat.com>
2025-03-28 13:47:11 -0500

    Initial UTM implementation

--- Machine.cs
+++ Machine.cs
@@ -3,35 +3,32 @@
 public class Machine
 {
     //Pointer manipulation
-    const byte IncrementMemoryPointer = 0;
-    const byte DecrementMemoryPointer = 1;
+    const byte IncrementMemoryPointer = 1;
     const byte IncrementNextProgramPointer = 2;
-    const byte DecrementNextProgramPointer = 3;
     
     //Information manipulation
-    const byte IncrementMemoryValue = 4;
-    const byte DecrementMemoryValue = 5;
+    const byte IncrementMemoryValue = 3;
 
     //Communication
-    const byte ReadInput = 6;
-    const byte WriteOutput = 7;
-    const byte CopyNextProgramToMemory = 8;
-    const byte CopyMemoryToNextProgram = 9;
+    const byte ReadInput = 4;
+    const byte WriteOutput = 5;
+    const byte CopyNextProgramToMemory = 6;
+    const byte CopyMemoryToNextProgram = 7;
 
     //Control flow
-    const byte JumpForward4 = 10;
-    const byte JumpBackward4 = 11;
-    const byte JumpForward16 = 12;
-    const byte JumpBackward16 = 13;
-    const byte JumpForward64 = 14;
-    const byte JumpBackward64 = 15;
-    const byte JumpForward256 = 16;
-    const byte JumpBackward256 = 17;
+    const byte JumpForward4 = 8;
+    const byte JumpBackward4 = 9;
+    const byte JumpForward16 = 10;
+    const byte JumpBackward16 = 11;
+    const byte JumpForward64 = 12;
+    const byte JumpBackward64 = 13;
+    const byte JumpForward256 = 14;
+    const byte JumpBackward256 = 15;
     
     //Halting
-    const byte Return = 18;
+    const byte Return = 16;
 
-    public const byte MaximumInstruction = 19;
+    public const byte MaximumInstruction = 17;
 
     public static void Execute(
         Span<byte> program, 
@@ -51,15 +48,69 @@ public class Machine
         while (true)
         {
             if (cycles > cycleLimit) { break; }
-            if (programCounter > program.Length - 1) { break; }
 
             switch (program[programCounter])
             {
-                //...
+                case IncrementMemoryPointer:
+                    memoryPointer++;
+                    memoryPointer %= memory.Length;
+                    break;
+                case IncrementNextProgramPointer:
+                    nextProgramPointer++;
+                    nextProgramPointer %= nextProgram.Length;
+                    break;
+                case IncrementMemoryValue:
+                    memory[memoryPointer]++;
+                    break;
+                case ReadInput:
+                    if (inputPointer < input.Length - 1)
+                    {
+                        memory[memoryPointer] = input[inputPointer++];
+                    }
+                    break;
+                case WriteOutput:
+                    if (outputPointer < output.Length - 1)
+                    {
+                        output[outputPointer++] = memory[memoryPointer];
+                    }
+                    break;
+                case CopyMemoryToNextProgram:
+                    nextProgram[nextProgramPointer] = memory[memoryPointer];
+                    break;
+                case CopyNextProgramToMemory:
+                    memory[memoryPointer] = nextProgram[nextProgramPointer];
+                    break;
+                case JumpForward4:
+                    programCounter += 4;
+                    break;
+                case JumpForward16:
+                    programCounter += 16;
+                    break;
+                case JumpForward64:
+                    programCounter += 64;
+                    break;
+                case JumpForward256:
+                    programCounter += 256;
+                    break;
+                case JumpBackward4:
+                    programCounter = (programCounter - 4 + program.Length) % program.Length;
+                    break;
+                case JumpBackward16:
+                    programCounter = (programCounter - 16 + program.Length) % program.Length;
+                    break;
+                case JumpBackward64:
+                    programCounter = (programCounter - 64 + program.Length) % program.Length;
+                    break;
+                case JumpBackward256:
+                    programCounter = (programCounter - 256 + program.Length) % program.Length;
+                    break;
+                case Return:
+                   return;
             }
 
             cycles++;
             programCounter++;
+            programCounter %= program.Length;
         }
     }
 }
